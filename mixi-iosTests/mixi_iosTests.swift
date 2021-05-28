8a6b7738ae4a8ceaa41cab9bccad80ee130fc31c
Kou Yamamoto <yama.121020@gmail.com>
2021-05-28 14:04:55 +0900

    Xcodeプロジェクトの追加

---
+++ mixi-iosTests/mixi_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  mixi_iosTests.swift
+//  mixi-iosTests
+//
+//  Created by kou yamamoto on 2021/05/28.
+//
+
+import XCTest
+@testable import mixi_ios
+
+class mixi_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
